36116dd93b1d34e75f9bd115d5636ad48ef11adc
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-01-10 22:03:00 +0530

    - fixed req.body issue

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const connectDB =  require('./config/database')
 const blogRoutes = require('./routes/blog')
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000;
+
+
 app.listen(PORT,()=>{
     console.log("app is running")
 })
