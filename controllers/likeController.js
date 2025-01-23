e4ff5fef70124bb5b5fb23b14157d2bf335f8f08
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-01-23 21:07:24 +0530

    fixed remove like controller issue

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -2,16 +2,24 @@ const Post = require('../models/postModel');
 const Like = require('../models/likeModel');
 exports.createLike = async (req,res) =>{
     try{
-        const {user, post} = req.body;
-        const like = await Like({user,post});
+
+        const {postId} = req.params;
+        const {user} = req.body;
+        const like = await Like({user,postId});
         const savedLike = await like.save()
-        const updatedPost = await Post.findByIdAndUpdate(post,{$push:{likes:savedLike._id}},{new:true}).populate('likes').exec();
-        
+        const updatedPost = await Post.findByIdAndUpdate(postId,{$push:{likes:savedLike._id}},{new:true}).populate({path:'likes' ,select:'user'}).exec();
+        if(!updatedPost){
+            return res.status(500).json({
+                success:false,
+                message:"failed to like",
+            })
+        }
         res.status(200).json({
             success:true,
             data:updatedPost,
             message:"like added successfully"
         })
+        
 
     }catch(error){
         res.status(500).json({
@@ -46,14 +54,25 @@ exports.getLikesByPostId = async (req, res) =>{
 
 exports.removeLike = async (req,res) =>{
     try{
-        const {id} = req.params;
-        const removedLike = await Like.findByIdAndDelete(id);
-        const updatedPost = await Post.findByIdAndUpdate(removedLike.post,{$pull:{likes:removedLike._id}},{new:true}).populate('likes').exec();
+        const {likeId,postId } = req.params;
+        console.log(likeId)
+        const likeToRemove = await Like.findById(likeId);
+        console.log(likeToRemove)
+        if (!likeToRemove) {
+            return res.status(404).json({
+                success: false,
+                message: "Like not found",
+            });
+        }
+        await Like.findByIdAndDelete(likeId)
+        const updatePost = await Post.findByIdAndUpdate(postId,{$pull:{likes:likeToRemove._id}},{new:true}).populate('likes');
+
         res.status(200).json({
             success:true,
-            data:updatedPost,
-            message:"like removed successfully"
+            data:updatePost,
+            message:'unliked post successfully'
         })
+
     }catch(error){
         res.status(500).json({
             success:false,
--- routes/blog.js
+++ routes/blog.js
@@ -16,13 +16,14 @@ router.delete('/:userId/delete',deleteUser)
 // comment controllers
 router.post('/posts/:postId/comment', createComment); // Create a comment
 router.get('/posts/:postId/comments', getCommentsByPostId); // Get comments for a post
+// deleting comment should also be removed from post collection
 router.delete('/comments/:id',deleteCommentById) // delete comment
 router.put('/comments/:id',updateCommentById) // delete comment
 
 // like controllers
 router.post('/posts/:postId/like', createLike);
 router.get('/posts/:postId/likes',getLikesByPostId);
-router.delete('/unlike/:id',removeLike);
+router.delete('/posts/:postId/likes/:likeId',removeLike);
 // router.get('/posts/:postId/like/:userId', checkUserLike);
 
 
@@ -31,6 +32,7 @@ router.post('/posts', createPost); // Create a post
 router.get('/posts', getAllPosts); // Get all posts
 router.get('/posts/:id', getPost); // Get a single post by ID
 router.put('/posts/:id', updatePost); // Update a post by ID
+// deleting posts should also be removed from user collection
 router.delete('/posts/:id', deletePost); // Delete a post by ID
 router.get('/:userId/posts',getAllUserPosts) //get all posts of user
 
